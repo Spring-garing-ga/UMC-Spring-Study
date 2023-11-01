ccf529c0935cff23673138e07689f2200b811845
Ga Dong Sik <ret0422@gmail.com>
2023-11-01 14:41:32 +0900

    Spring init

---
+++ src/main/java/umc/study/spring/SpringGaringGaApplication.java
@@ -0,0 +1,13 @@
+package umc.study.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGaringGaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGaringGaApplication.class, args);
+	}
+
+}
