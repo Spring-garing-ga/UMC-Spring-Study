57233b5d2384b864dd103df390679a17be0938ff
Ga Dong Sik <ret0422@gmail.com>
2023-11-04 21:51:34 +0900

    Mission & UserMission Entity Create

--- src/main/java/umc/study/SpringGaringGaApplication.java
+++ src/main/java/umc/study/SpringGaringGaApplication.java
@@ -1,9 +1,11 @@
-package umc.study.spring;
+package umc.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringGaringGaApplication {
 
 	public static void main(String[] args) {
