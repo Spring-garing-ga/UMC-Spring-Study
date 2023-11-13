8842684177f4783e6700f4d9f3cd1352faad8b88
KongJunWoo <joonoo3@naver.com>
2023-11-13 14:29:05 +0900

    feat : 에러 처리

---
+++ src/main/java/umc/study/spring/domain/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,9 @@
+package umc.study.spring.domain.apiPayload.code;
+
+public interface BaseErrorCode {
+
+    public ErrorReasonDTO getReason();
+
+    public ErrorReasonDTO getReasonHttpStatus();
+
+}
---
+++ src/main/java/umc/study/spring/domain/service/TempService/TempQueryService.java
@@ -0,0 +1,7 @@
+package umc.study.spring.domain.service.TempService;
+
+public interface TempQueryService {
+
+    void CheckFlag(Integer flag);
+
+}
